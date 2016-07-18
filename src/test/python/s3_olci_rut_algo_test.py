9c608f6b8078bc7278b84afbe1a4df696c9fc98a
Sam Hunt <samedwardhunt@gmail.com>
2016-07-18 14:56:49 +0200

    Update and rename s2_rut_algo_test.py to s3_olci_rut_algo_test.py
    Began to rewrite s2_rut_algo_test.py to s3_olci_rut_algo_test.py

---
+++ src/test/python/s3_olci_rut_algo_test.py
@@ -0,0 +1,9 @@
+import unittest
+import s3_olci_rut_algo as s3_olci_rut_algo
+import numpy as np
+
+
+class S3OLCIRutAlgoTest(unittest.TestCase):
+    def test_simple_case(self):
+        #tbw
+        return 0
